b0d487910637022c529ba076b0094aa278a15734
Dhruv Dhawan <dhruvdhawan11@gmail.com>
2022-04-11 09:49:41 -0700

    Added text area

--- src/app.js
+++ src/app.js
@@ -5,3 +5,23 @@ const notes = [
     id: 1 
   }
 ]
+
+const addBtn = document.getElementsByClassName("icons")[0]
+const notesArea = document.getElementsByClassName("write-note-area")[0]
+const notesListContainer = document.getElementsByClassName("notes-list")[0]
+
+addBtn.addEventListener("click", () => createNewNote())
+
+function createNewNote() {
+    const newNote = `
+    <div class="newnote__container"> 
+    <input class= "title" type="text" placeholder="Title..."/> 
+    <textarea class="notebody" placeholder="Note..."></textarea>
+       <div class="newnote__buttons">
+          <button onClick="saveNote(this)" class="save">Save</button>
+          <button onClick="deleteNote(this)" class="cancel">Cancel</button>
+       </div>
+    </div>
+    `
+    notesArea.innerHTML=newNote
+}
